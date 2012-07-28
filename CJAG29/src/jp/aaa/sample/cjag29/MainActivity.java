399cd8b3004b847472dff5b08af2af565a6c8187
yamasuki- <yamasaki.yasunori@gmail.com>
2012-07-28 16:06:47 +0900

    sampleです

---
+++ CJAG29/src/jp/aaa/sample/cjag29/MainActivity.java
@@ -0,0 +1,40 @@
+package jp.aaa.sample.cjag29;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.View.OnLongClickListener;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button btn01 = (Button)findViewById(R.id.btn01);
+        btn01.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Toast.makeText(getApplicationContext(), "ボタン1クリック('A`)", Toast.LENGTH_SHORT).show();
+			}
+		});
+        Button btn02 = (Button)findViewById(R.id.btn02);
+        btn02.setOnLongClickListener(new OnLongClickListener() {
+			@Override
+			public boolean onLongClick(View v) {
+				Toast.makeText(getApplicationContext(), "ボタン2クリック('A`)", Toast.LENGTH_SHORT).show();
+				return false;
+			}
+		});
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_main, menu);
+        return true;
+    }
+}
