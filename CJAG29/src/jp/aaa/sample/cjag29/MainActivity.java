eeda3214b6e6c6f5d9424761a2a50cb3956f9c2f
yamasuki- <yamasaki.yasunori@gmail.com>
2012-07-28 16:14:57 +0900

    local commit sample

--- CJAG29/src/jp/aaa/sample/cjag29/MainActivity.java
+++ CJAG29/src/jp/aaa/sample/cjag29/MainActivity.java
@@ -11,6 +11,7 @@ import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
